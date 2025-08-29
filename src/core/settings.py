4661dd5433172f9100edb56e9f6ac098a21d3b1d
Mac <mac@MacBook-Air.local>
2025-08-29 10:24:58 +0300

    Change DB

--- src/core/settings.py
+++ src/core/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sadik_pavlusha',
         'USER': 'root',
-        'PASSWORD': 'Andrii1212',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
